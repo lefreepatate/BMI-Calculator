a39e5166a2d145e46dda7f7e55d9a0fe86187eef
lefreepatate <c_garcia_muskat@hotmail.com>
2020-04-10 16:51:06 +0200

    Adding firsts IBOutlets/IBActions and calculating BMI

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: IBOUTLETS
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    // MARK: IBACTIONS
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightLabel.text = String(format:"%.2f", sender.value) + "m"
+    }
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))Kg"
+    }
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let bmi = weightSlider.value / pow(heightSlider.value, 2)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
